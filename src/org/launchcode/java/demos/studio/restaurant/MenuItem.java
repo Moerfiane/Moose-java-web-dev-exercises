20f767318835686c3e41ec859cefc0ef1673ec55
amanteiga <alicia.manteiga@gmail.com>
2021-01-11 19:57:37 -0600

    second commit; Alicia finished constructor, one get/set on MenuItem

--- src/org/launchcode/java/demos/studio/restaurant/MenuItem.java
+++ src/org/launchcode/java/demos/studio/restaurant/MenuItem.java
@@ -5,13 +5,22 @@ public class MenuItem {
     private String name;
     private String description;
     private float price;
-    private String category;
+    private arrayList<String> category;
     private String date;
 
     public MenuItem(String name, String description, float price, String category, String date) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
 
     }
 
-
+    public String getName() {
+        return name;
+    }
+    public void setName(String, aName) {
+        name = aName;
+    }
 
 }
