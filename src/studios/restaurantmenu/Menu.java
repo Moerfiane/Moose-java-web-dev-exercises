a2831a03f9157a49097b55b52533f605d314a220
cdelonga <74030958+cdelonga@users.noreply.github.com>
2021-01-11 20:23:01 -0600

    caroline made changes

--- src/studios/restaurantmenu/Menu.java
+++ src/studios/restaurantmenu/Menu.java
@@ -1,4 +1,21 @@
 package studios.restaurantmenu;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Menu {
+    Menu newMenu = new Menu;
+
+    private Date dateUpdated;
+    private ArrayList<MenuItem> items = new ArrayList<>();
+    private boolean isUpdated = true;
+
+    public double getMenu () {
+
+    }
+
+    public Menu(Date aDate, ArrayList<MenuItem> anItem) {
+        this.dateUpdated = aDate;
+        this.items = anItem;
+    }
+
 }
--- src/studios/restaurantmenu/MenuItem.java
+++ src/studios/restaurantmenu/MenuItem.java
@@ -1,4 +1,31 @@
 package studios.restaurantmenu;
 
 public class MenuItem {
-}
+    //MenuItem newMenuItem = new MenuItem;
+
+    //instance fields
+    private double price ;
+    private String name;
+    private String category;
+    private String description;
+    boolean isNew;
+
+    //constructor
+    public MenuItem(double price, String description, String category, String name, boolean isNew) {
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.name = name;
+        this.isNew = isNew;
+
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void isNew(boolean isNew) {
+        this.isNew = isNew;
+    }
+
+        }
+
