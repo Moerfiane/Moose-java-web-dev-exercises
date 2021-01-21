9e21dd4437c7adace8e6f4a22f5a7fe2eb6882f5
Meg <40369841+mcullins@users.noreply.github.com>
2021-01-21 17:47:13 -0600

    added menu constructor, getters and setters

--- src/org/launchcode/java/studios/restaurant/Menu.java
+++ src/org/launchcode/java/studios/restaurant/Menu.java
@@ -6,6 +6,24 @@ public class Menu {
     public Date newestMenuDate;
     public MenuItem menuItem;
 
+    public Menu(Date newestMenuDate, MenuItem menuItem) {
+        this.newestMenuDate = newestMenuDate;
+        this.menuItem = menuItem;
+    }
 
+    public Date getNewestMenuDate() {
+        return newestMenuDate;
+    }
 
+    public void setNewestMenuDate(Date newestMenuDate) {
+        this.newestMenuDate = newestMenuDate;
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+    }
 }
