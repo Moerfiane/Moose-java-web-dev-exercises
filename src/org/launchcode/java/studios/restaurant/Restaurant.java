9868e96397dfad31318f6930ece6f918b6271a5b
Rumple1981 <eric@koehn.net>
2021-01-21 20:18:10 -0600

    Eric added menu items

--- src/org/launchcode/java/studios/restaurant/Restaurant.java
+++ src/org/launchcode/java/studios/restaurant/Restaurant.java
@@ -1,7 +1,13 @@
 package org.launchcode.java.studios.restaurant;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Restaurant {
     public static void main(String[] args) {
-
+        ArrayList<MenuItem>arrayList = new ArrayList(Arrays.asList("Cheese", "Meat", "Sugar"));
+        //arrayList<Menu>arrayList = new Menu(addItem());
+        System.out.println(arrayList);
     }
 }
