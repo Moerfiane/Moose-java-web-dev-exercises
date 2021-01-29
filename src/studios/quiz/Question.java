59071538c89a6a4c9324749786051b7068c1def3
Eskindir <eskindiraleb@yahoo.com>
2021-01-28 20:10:31 -0600

    Eskindir true false class

--- src/studios/quiz/Question.java
+++ src/studios/quiz/Question.java
@@ -1,13 +1,25 @@
 package studios.quiz;
 import java.util.Scanner;
 
-public class Question {
+public abstract class Question {
 
     private String prompt;
     private String response;
     private String correctAnswer;
 
-    public Question(String prompt, String response, String correctAnswer)
+    public Question(String prompt, String response, String correctAnswer){
+        this.prompt = prompt;
+        this.response = response;
+        this.correctAnswer = correctAnswer;
+    }
 
-
+    public String getPrompt() {
+        return prompt;
+    }
+    public String getResponse() {
+        return response;
+    }
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
 }
