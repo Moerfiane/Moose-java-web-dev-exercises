e63c7f1c42f5206e386ebe527f778dbf064aba91
farleysm8909 <farleysm8909@uwec.edu>
2021-02-01 17:47:10 -0600

    Sam created starter code for disc studio (interfaces)

---
+++ src/org/launchcode/java/studios/spinningthediscs/CD.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.studios.spinningthediscs;
+
+public class CD {
+
+    // TODO: Implement your custom interface.
+
+    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
+    //  need to be declared separately.
+
+}
